2a2938467bc4aaf8bcb03728a52845e780a124e9
Justin <heartleyjm@gmail.com>
2023-01-11 19:46:03 -0500

    FEAT: add post-streak name submission
    * arranged webpage to follow flow of game
    * added leaderboard table
    * moved Player vs CPU to the top
    * added current streak to page
    * added name input section
    * made name input come up after a streak in broken, in PvC mode
    * call updateTopScore after name submission
    * renamed resetGame to resetBoard
    * renamed getTopScore to updateTopScore
    * renamed showSplashScreen to revealSplashScreen
    * added play again or back to menu options, post-game

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 $(function () {
-    getTopScore();
-    resetGame();
-    showSplashScreen();
+    updateTopScore();
+    revealSplashScreen();
     setUpEventListeners();
 });
 
@@ -30,10 +29,9 @@ let gameType;
 let isXTurn;
 let isGameOver;
 let spaces;
-let sessionId;
-let currentStreak;
+let currentStreak = 0;
 
-function showSplashScreen() {
+function revealSplashScreen() {
     $("#splash").fadeIn(1200);
     setTimeout(function () {
         $("#splash").fadeOut(300);
@@ -41,19 +39,15 @@ function showSplashScreen() {
     }, 2500);
 }
 
-function resetGame() {
+function resetBoard() {
     if (isGameOver) {
         const gameSpaces = document.getElementsByClassName("space");
         for (let i = 0; i < gameSpaces.length; i++) {
             gameSpaces[i].textContent = "";
             gameSpaces[i].style.backgroundColor = defaultBackgroundColor;
         }
-
-        $("#reset").hide();
-
-        $("#board").hide(300);
-        $("#menu").fadeIn(1000);
     }
+    $("#boardMenu").hide();
     message.innerHTML = startMessage;
     gameType = "";
     isXTurn = true;
@@ -62,8 +56,11 @@ function resetGame() {
 }
 
 function setUpEventListeners() {
-    $("#menu p").click(function () {
-        gameSelect(this);
+    $("#playerVsCPU").click(function () {
+        gameStart(this.textContent);
+    });
+    $("#playerVsPlayer").click(function () {
+        gameStart(this.textContent);
     });
     $(".space.c1.r1").click(function () {
         playerMove(0, this);
@@ -92,15 +89,34 @@ function setUpEventListeners() {
     $(".space.c3.r3").click(function () {
         playerMove(8, this);
     });
-    $("#reset").click(function () {
-        resetGame();
+    $("#playAgain").click(function () {
+        gameStart(gameType);
+    });
+
+    $(".backToMenu").click(function () {
+        updateCurrentStreak(0);
+        showOnly("#menu");
+    });
+
+    $("#submit").click(async function () {
+        const name = $("#name").val();
+        if (name == "") {
+            alert("Please enter a name!");
+            return;
+        }
+
+        const scores = await addScoreToLeaderboard(name);
+        goToLeaderboard(scores, name);
+
+        updateTopScore();
+    });
     });
 }
 
-function gameSelect(htmlElement) {
-    gameType = htmlElement.textContent;
-    $("#board").fadeIn(1000);
-    $("#menu").hide(300);
+function gameStart(selectedGameType) {
+    resetBoard();
+    gameType = selectedGameType;
+    showOnly("#board");
 }
 
 function checkForGameOver() {
@@ -108,20 +124,47 @@ function checkForGameOver() {
         if (spaces[winSet[0]]
             && spaces[winSet[0]] == spaces[winSet[1]]
             && spaces[winSet[1]] == spaces[winSet[2]]) {
+
             message.innerHTML = (isXTurn ? "X" : "O") + " Wins!";
             isGameOver = true;
-            $("#reset").show();
+            $("#boardMenu").show();
+
+            if (gameType == "Player vs CPU") {
+                if (isXTurn) {
+                    message.innerHTML = "You won!";
+                    currentStreak++;
+                    updateCurrentStreak(currentStreak);
+                }
+                else {
+                    message.innerHTML = "You lost :(";
+                    if (currentStreak > 0) {
+                        goToNameInput();
+                    } else {
+                        updateCurrentStreak(0);
+                    }
+                }
+            }
         }
         const movesLeft = spaces.filter(space => space === "").length;
         if (!isGameOver && movesLeft == 0) {
             message.innerHTML = "It's a Tie!";
             isGameOver = true;
-            $("#reset").show();
+            $("#boardMenu").show();
+            if (gameType == "Player vs CPU") {
+                if (currentStreak > 0) {
+                    goToNameInput();
+                } else {
+                    updateCurrentStreak(0);
+                }
+            }
         }
     });
 }
 
 function playerMove(spaceIndex, htmlElement) {
+    if (htmlElement.textContent) {
+        return;
+    }
     move(spaceIndex, htmlElement);
     if (gameType == "Player vs CPU" && !isGameOver) {
         computerMove();
@@ -199,8 +242,18 @@ function findIndexForBestMove(playerMark) {
         }
     });
     return bestMove;
+}
 
-function getTopScore() {
+function showOnly(id) {
+    $("#board").hide(300);
+    $("#leaderboard").hide(300);
+    $("#menu").hide(300);
+    $("#nameInput").hide(300);
+
+    $(id).fadeIn(1000);
+}
+
+function updateTopScore() {
     dreamlo.initialize(publicKey, privateKey, useHttps);
 
     const skip = 0;
@@ -226,3 +279,60 @@ function getTopScore() {
         });
 }
 
+function goToNameInput() {
+    let text = "You had a final streak of ";
+    if (currentStreak == 1) {
+        text += "1 game";
+    } else {
+        text += currentStreak + " games";
+    }
+    text += "! \n Do you want to add your name to the leaderboard?";
+    $("#nameInputMessage").text(text);
+
+    showOnly("#nameInput");
+    $("#nameInput input").focus();
+}
+
+function addScoreToLeaderboard(name) {
+    const timestamp = new Date().getTime();
+    const score = {
+        name: timestamp,
+        points: currentStreak,
+        text: name
+    };
+
+    return dreamlo.addScore(score, dreamlo.ScoreFormat.Object, dreamlo.SortOrder.PointsDescending, true).then((scores) => {
+        return scores;
+    })
+        .catch((error) => {
+            alert(error);
+        });
+}
+
+function goToLeaderboard(scores, nameToHighlight) {
+    $("#leaderboard tbody").empty();
+
+    let leaderboardBody = "";
+    if (scores.length == 0) {
+        leaderboardBody = "<tr><td colspan='3'>No scores yet!</td></tr>";
+    } else {
+        for (let index = 0; index < scores.length; index++) {
+
+            if (nameToHighlight && nameToHighlight === scores[index].text) {
+                leaderboardBody += "<tr class='highlight'>";
+            }
+            else {
+                leaderboardBody += "<tr>";
+            }
+            leaderboardBody += "<td>" + (index + 1) + "</td><td>" + scores[index].text + "</td><td>" + scores[index].score + "</td></tr>";
+        }
+    }
+    $("#leaderboard tbody").append(leaderboardBody);
+    showOnly("#leaderboard");
+}
+
+function updateCurrentStreak(number) {
+    console.log("updateCurrentStreak:" + number);
+    currentStreak = number;
+    $("#currentStreak").text("CURRENT STREAK: " + currentStreak);
+}
