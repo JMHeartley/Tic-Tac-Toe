e140b2f7745d8890ca36008aebccb3138eb5581c
Justin <heartleyjm@gmail.com>
2023-01-12 18:10:47 -0500

    REFACTOR: update naming scheme for constants

--- script.js
+++ script.js
@@ -4,14 +4,16 @@ $(function () {
     setUpEventListeners();
 });
 
-const message = document.getElementById("message");
-const startMessage = "X Always Goes First!";
-const defaultBackgroundColor = "#40E0D0";
-const xBackgroundColor = "#15EE16";
-const xFontColor = "#2E8B57";
-const oBackgroundColor = "#F6546A";
-const oFontColor = "#C92C2C";
-const winSets = [
+const PUBLIC_KEY = "5fa8af3feb371a09c4c51c2f";
+const PRIVATE_KEY = "gIJe5J6z20ik-5Mm_NDCyAzldYBBzTmUub-VeuQrX1jw";
+const USE_HTTPS = true;
+const START_MESSAGE = "X Always Goes First!";
+const DEFAULT_BACKGROUND_COLOR = "#40E0D0";
+const X_BACKGROUND_COLOR = "#15EE16";
+const X_FONT_COLOR = "#2E8B57";
+const O_BACKGROUND_COLOR = "#F6546A";
+const o_FONT_COLOR = "#C92C2C";
+const WIN_SETS = [
     [0, 1, 2],
     [3, 4, 5],
     [6, 7, 8],
@@ -21,9 +23,6 @@ const winSets = [
     [0, 4, 8],
     [6, 4, 2]
 ];
-const publicKey = "5fa8af3feb371a09c4c51c2f";
-const privateKey = "gIJe5J6z20ik-5Mm_NDCyAzldYBBzTmUub-VeuQrX1jw";
-const useHttps = true;
 
 let gameType;
 let isXTurn;
@@ -44,11 +43,11 @@ function resetBoard() {
         const gameSpaces = document.getElementsByClassName("space");
         for (let i = 0; i < gameSpaces.length; i++) {
             gameSpaces[i].textContent = "";
-            gameSpaces[i].style.backgroundColor = defaultBackgroundColor;
+            gameSpaces[i].style.backgroundColor = DEFAULT_BACKGROUND_COLOR;
         }
     }
     $("#boardMenu").hide();
-    message.innerHTML = startMessage;
+    $("#message").innerHTML = START_MESSAGE;
     gameType = "";
     isXTurn = true;
     isGameOver = false;
@@ -123,23 +122,23 @@ function gameStart(selectedGameType) {
 }
 
 function checkForGameOver() {
-    winSets.forEach((winSet) => {
+    WIN_SETS.forEach((winSet) => {
         if (spaces[winSet[0]]
             && spaces[winSet[0]] == spaces[winSet[1]]
             && spaces[winSet[1]] == spaces[winSet[2]]) {
 
-            message.innerHTML = (isXTurn ? "X" : "O") + " Wins!";
+            $("#message").innerHTML = (isXTurn ? "X" : "O") + " Wins!";
             isGameOver = true;
             $("#boardMenu").show();
 
             if (gameType == "Player vs CPU") {
                 if (isXTurn) {
-                    message.innerHTML = "You won!";
+                    $("#message").innerHTML = "You won!";
                     streak++;
                     updateCurrentStreak(streak);
                 }
                 else {
-                    message.innerHTML = "You lost :(";
+                    $("#message").innerHTML = "You lost :(";
                     if (streak > 0) {
                         goToNameInput();
                     } else {
@@ -150,7 +149,7 @@ function checkForGameOver() {
         }
         const movesLeft = spaces.filter(space => space === "").length;
         if (!isGameOver && movesLeft == 0) {
-            message.innerHTML = "It's a Tie!";
+            $("#message").innerHTML = "It's a Tie!";
             isGameOver = true;
             $("#boardMenu").show();
             if (gameType == "Player vs CPU") {
@@ -179,11 +178,11 @@ function move(spaceIndex, htmlElement) {
         spaces[spaceIndex] = isXTurn ? "X" : "O";
 
         if (isXTurn) {
-            $(htmlElement).css("color", xFontColor);
-            $(htmlElement).css("background", xBackgroundColor);
+            $(htmlElement).css("color", X_FONT_COLOR);
+            $(htmlElement).css("background", X_BACKGROUND_COLOR);
         } else {
-            $(htmlElement).css("color", oFontColor);
-            $(htmlElement).css("background", oBackgroundColor);
+            $(htmlElement).css("color", o_FONT_COLOR);
+            $(htmlElement).css("background", O_BACKGROUND_COLOR);
         }
 
         htmlElement.textContent = isXTurn ? "X" : "O";
@@ -192,7 +191,7 @@ function move(spaceIndex, htmlElement) {
 
         if (!isGameOver) {
             isXTurn = !isXTurn;
-            message.innerHTML = (isXTurn ? "X" : "O") + "'s Turn!";
+            $("#message").innerHTML = (isXTurn ? "X" : "O") + "'s Turn!";
         }
     }
 }
@@ -227,7 +226,7 @@ function findIndexForBestMove(playerMark) {
     //set index of best move to variable and return that
     //forEach's function boundary inhibits proper return statements
     let bestMove;
-    winSets.forEach((winSet) => {
+    WIN_SETS.forEach((winSet) => {
         if (spaces[winSet[0]] == playerMark
             && spaces[winSet[0]] == spaces[winSet[1]]
             && spaces[winSet[2]] == "") {
@@ -257,7 +256,7 @@ function showOnly(id) {
 }
 
 function updateTopScore() {
-    dreamlo.initialize(publicKey, privateKey, useHttps);
+    dreamlo.initialize(PUBLIC_KEY, PRIVATE_KEY, USE_HTTPS);
 
     const skip = 0;
     const take = 1;
